5ae03995199ab93c93524b4c225850e1212731aa
Bryan Ashby <bryan@l33t.codes>
2015-04-23 21:42:57 -0600

    * More package.json updates & some logging early failures

--- core/logger.js
+++ core/logger.js
@@ -4,12 +4,33 @@
 var bunyan		= require('bunyan');
 var miscUtil	= require('./misc_util.js');
 var paths		= require('path');
-var conf		= require('./config.js');
+var fs			= require('fs');
 
 module.exports	= {
 	init	: function() {
+		var Config = require('./config.js').config;
 		//var ringBufferLimit = miscUtil.valueWithDefault(config.logRingBufferLimit, 100);
-		var logPath			= miscUtil.valueWithDefault(conf.config.paths.logs);
+		var logPath			= Config.paths.logs;
+
+		//
+		//	Create something a bit more friendly if the log directory cannot be used
+		//
+		//	:TODO: this seems cheesy...
+		var logPathError;
+		try {
+			var pathStat = fs.statSync(logPath);
+			if(!pathStat.isDirectory()) {
+				logPathError = logPath + ' is not a directory!';
+			}
+		} catch(e) {
+			logPathError = e.message;
+		}
+
+		if(logPathError) {
+			console.error(logPathError);
+			process.exit();
+		}
+
 		var logFile			= paths.join(logPath, 'enigma-bbs.log');
 
 		//	:TODO: make this configurable --
