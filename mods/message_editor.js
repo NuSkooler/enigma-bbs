8c7d609abb2daebb2b4d4f7cfc848290b754df55
Bryan Ashby <bryan@l33t.codes>
2015-06-24 23:09:26 -0600

    * Start skeleton work on message editor module

--- mods/message_editor.js
+++ mods/message_editor.js
@@ -3,6 +3,10 @@
 
 var MenuModule		= require('../core/menu_module.js').MenuModule;
 
+var async			= require('async');
+var assert			= require('assert');
+var _				= require('lodash');
+
 exports.getModule	= MessageEditorModule;
 
 exports.moduleInfo = {
@@ -11,8 +15,34 @@ exports.moduleInfo = {
 	author	: 'NuSkooler',
 };
 
-function MessageEditorModule(menuConfig) {
-	MenuModule.call(this, menuConfig);
+function MessageEditorModule(options) {
+	MenuModule.call(this, options);
+
+	var self = this;
+	var args = options.menuConfig.args;
+
+	this.initSequence = function() {
+		async.waterfall(
+			[
+				function beforeDisplayArt(callback) {
+					self.beforeArt();
+					callback(null);
+				},
+				function displayHeader(callback) {
+					if(_.isString(args.art.header)) {
+						self.displayArtAsset(args.art.header, function hdrDisplayed(err, mciMap) {
+
+						});
+					}
+				}
+			]
+		);	
+	};
 }
 
 require('util').inherits(MessageEditorModule, MenuModule);
+
+MessageEditorModule.prototype.enter = function(client) {	
+	MessageEditorModule.super_.prototype.enter.call(this, client);
+};
+
