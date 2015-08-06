a0a97350fea414e5d7b9c51932c1596a85d43347
Bryan Ashby <bryan@l33t.codes>
2015-08-06 16:25:52 -0600

    * Add l33t.codes URL to connect banner

--- core/connect.js
+++ core/connect.js
@@ -87,8 +87,8 @@ function prepareTerminal(term) {
 function displayBanner(term) {
 	//	:TODO: add URL(s) to banner
 	term.write(colorCodes.pipeToAnsi(util.format(
-		'|33Conected to |32EN|33|01i|00|32|22GMA|32|01½|00 |33BBS version|31|01 %s\n' +
-		'|00|33Copyright (c) 2014-2015 Bryan Ashby\n' + 
+		'|33Conected to |32EN|33|01i|00|32|22GMA|32|01½|00 |33BBS version|31|01 %s\n'	+
+		'|00|33Copyright (c) 2014-2015 Bryan Ashby |33|01- |31|01http://l33t.codes/\n'	+ 
 		'|00', packageJson.version)));
 }
 
