6fa19f9ac3f7fae5b1c022b97a69cac019b6bc11
Bryan Ashby <bryan@l33t.codes>
2016-08-26 21:29:49 -0600

    assert() that won't expload when not in 'dev' mode

---
+++ core/enigma_assert.js
@@ -0,0 +1,18 @@
+/* jslint node: true */
+'use strict';
+
+//	ENiGMA½
+const Config	= require('./config.js').config;
+const Log		= require('./logger.js').log;		
+
+//	deps
+const assert	= require('assert');
+
+module.exports = function(condition, message) {
+	if(Config.debug.assertsEnabled) {
+		assert.apply(this, arguments);	
+	} else if(!(condition)) {
+		const stack = new Error().stack;
+		Log.error( { condition : condition, stack : stack }, message || 'Assertion failed' );
+	}
+};
