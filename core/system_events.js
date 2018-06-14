c08e4dbe046b0f4b0d6732cb51c4b60da41d7fae
Bryan Ashby <bryan@l33t.codes>
2018-06-13 20:54:59 -0600

    New system events

--- core/system_events.js
+++ core/system_events.js
@@ -6,12 +6,17 @@ module.exports = {
 	ClientDisconnected	: 'codes.l33t.enigma.system.disconnected',		//	{ client, connectionCount }
 	TermDetected		: 'codes.l33t.enigma.system.term_detected',		//	{ client }
 
+	ThemeChanged		: 'codes.l33t.enigma.system.theme_changed',		//	{ themeId }
+	ConfigChanged		: 'codes.l33t.enigma.system.config_changed',
+	MenusChanged		: 'codes.l33t.enigma.system.menus_changed',
+	PromptsChanged		: 'codes.l33t.enigma.system.prompts_changed',
+
 	//	User - includes { user, ...}
 	NewUser				: 'codes.l33t.enigma.system.new_user',
 	UserLogin			: 'codes.l33t.enigma.system.user_login',
 	UserLogoff			: 'codes.l33t.enigma.system.user_logoff',
-	UserUpload			: 'codes.l33t.enigma.system.user_upload',	//	{..., files[ fileEntry, ...] }
-	UserDownload		: 'codes.l33t.enigma.system.user_download',	//	{..., files[ fileEntry, ...] }
+	UserUpload			: 'codes.l33t.enigma.system.user_upload',		//	{..., files[ fileEntry, ...] }
+	UserDownload		: 'codes.l33t.enigma.system.user_download',		//	{..., files[ fileEntry, ...] }
 
 	//	NYI below here:
 	UserPostMessage		: 'codes.l33t.enigma.system.user_post_msg',
