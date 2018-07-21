c1ae3d88bad3fcdec4e328ff926ae42cdbec7788
Bryan Ashby <bryan@l33t.codes>
2018-07-21 14:28:18 -0600

    * Fix RunDoor event name * Standardize *.user_* event names

--- core/system_events.js
+++ core/system_events.js
@@ -12,14 +12,12 @@ module.exports = {
     PromptsChanged      : 'codes.l33t.enigma.system.prompts_changed',   //  (prompt.hjson)
 
     //  User - includes { user, ...}
-    NewUser             : 'codes.l33t.enigma.system.new_user',
+    NewUser             : 'codes.l33t.enigma.system.user_new',
     UserLogin           : 'codes.l33t.enigma.system.user_login',
     UserLogoff          : 'codes.l33t.enigma.system.user_logoff',
     UserUpload          : 'codes.l33t.enigma.system.user_upload',       //  {..., files[ fileEntry, ...] }
     UserDownload        : 'codes.l33t.enigma.system.user_download',     //  {..., files[ fileEntry, ...] }
-
-    //  NYI below here:
-    UserPostMessage     : 'codes.l33t.enigma.system.user_post_msg',
+    UserPostMessage     : 'codes.l33t.enigma.system.user_post_msg',     //  { areaTag }
     UserSendMail        : 'codes.l33t.enigma.system.user_send_mail',
-    UserSendRunDoor     : 'codes.l33t.enigma.system.user_run_door',
+    UserRunDoor         : 'codes.l33t.enigma.system.user_run_door',
 };
