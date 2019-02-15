66bd2ce10849039b1a6912e466287b6029b6c19d
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2019-02-15 10:15:37 -0800

    Downgraded to AES128
    Downgraded to AES128, AES256 does not currently seem to work.

--- core/config.js
+++ core/config.js
@@ -218,7 +218,7 @@ function getDefaultConfig() {
 				//	Private key in PEM format
 				//
 				//	Generating your PK:
-				//	> openssl genrsa -aes256 -out ./config/ssh_private_key.pem 2048
+				//	> openssl genrsa -aes128 -out ./config/ssh_private_key.pem 2048
 				//
 				//	Then, set servers.ssh.privateKeyPass to the password you use above
 				//	in your config.hjson
