c9b3c9bc412f807fe09a87b4a7794e39654fa2ac
Bryan Ashby <bryan@l33t.codes>
2023-02-06 22:18:09 -0700

    Better context check

--- core/activitypub/object.js
+++ core/activitypub/object.js
@@ -4,6 +4,8 @@ const Endpoints = require('./endpoint');
 // deps
 const { isString, isObject } = require('lodash');
 
+const Context = '@context';
+
 module.exports = class ActivityPubObject {
     constructor(obj, withContext = [ActivityStreamsContext]) {
         if (withContext) {
@@ -39,16 +41,31 @@ module.exports = class ActivityPubObject {
     }
 
     isValid() {
-        const nonEmpty = s => isString(s) && s.length > 1;
-        //  :TODO: Additional validation
-        if (
-            (this['@context'] === ActivityStreamsContext ||
-                this['@context'][0] === ActivityStreamsContext) &&
-            nonEmpty(this.id) &&
-            nonEmpty(this.type)
-        ) {
-            return true;
+        //
+        //  If @context is present, it must be valid;
+        //  child objects generally inherit, so they may not have one
+        //
+        if (this[Context]) {
+            if (!this.isContextValid()) {
+                return false;
+            }
         }
+
+        const checkString = s => isString(s) && s.length > 1;
+        return checkString(this.id) && checkString(this.type);
+    }
+
+    isContextValid() {
+        if (Array.isArray(this[Context])) {
+            if (this[Context][0] === ActivityStreamsContext) {
+                return true;
+            }
+        } else if (isString(this[Context])) {
+            if (ActivityStreamsContext === this[Context]) {
+                return true;
+            }
+        }
+
         return false;
     }
 
